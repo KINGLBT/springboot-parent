5c9476d23416727457cbc40a216342f7bbbe6d0c
luomeng <lmluomeng1992@163.com>
2019-07-20 21:10:24 +0800

    SpringBoot Auto-configuration 注解使用

---
+++ springboot-chapter03/src/main/java/com/rebote/springboot/Application.java
@@ -0,0 +1,21 @@
+package com.rebote.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: Da Shuai
+ * @Date: 2019/7/19 16:21
+ * @Description:
+ * @Company: zytech
+ * @Email: 1043489207@qq.com
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+
+}
