3ea05a2605b253c3ee1ee457c5b728801c29d58a
lmluo <lmluomeng1992@163.com>
2019-08-23 15:11:47 +0800

    '提交'

--- springboot-chapter05/src/main/java/com/rebote/springboot/Application.java
+++ springboot-chapter05/src/main/java/com/rebote/springboot/Application.java
@@ -14,7 +14,7 @@ public class Application {
         SpringApplication app = new SpringApplication(Application.class);
         //app.setBannerMode(Banner.Mode.OFF);
         // 添加监听器,必须放在run之前
-        //放大手动阀
+        //放大手动阀fdsaa
         //app.addListeners(new ApplicationPreparedEventListener(),new ApplicationEnvironmentPreparedEventListener(),new ApplicationStartingEventListener());
         app.run(args);
     }
